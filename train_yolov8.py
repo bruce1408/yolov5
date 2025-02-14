e7024ea4480fe0cfa346d615f51ba86a17f9e7e0
bruce1408 <summer56567@163.com>
2025-02-14 14:43:54 +0800

    feat: 修改训练参数，使用上次训练的权重继续训练

--- train_yolov8.py
+++ train_yolov8.py
@@ -1,16 +1,21 @@
 import os
 from ultralytics import YOLO
-os.environ['CUDA_VISIBLE_DEVICES'] = '2'
+os.environ['CUDA_VISIBLE_DEVICES'] = '7'
 
 # 加载YOLOv8模型
-model = YOLO('/mnt/share_disk/bruce_trie/yolov5/yolov8s.pt')  # 你可以选择不同的模型权重文件
+# model = YOLO('/mnt/share_disk/bruce_trie/yolov5/yolov8n.pt')  # 你可以选择不同的模型权重文件
+model = YOLO('/mnt/share_disk/bruce_trie/runs/detect/train13/weights/last.pt')  # 你可以选择不同的模型权重文件
 
+# 获取上次训练的epoch数
+# 可以从模型对象中获取训练信息
+last_epoch = model.ckpt['epoch'] if hasattr(model, 'ckpt') else 0
+current_epoch = 100 - last_epoch 
 # 设置训练参数
 train_params = {
     'data': '/mnt/share_disk/bruce_trie/yolov5/data/coco.yaml',  # 数据集配置文件路径
-    'epochs': 100,  # 训练轮数
+    'epochs': current_epoch,  # 训练轮数
     'imgsz': 640,  # 输入图像大小
-    'device': '2',  # 使用的GPU设备编号
+    'device': '7',  # 使用的GPU设备编号
     'batch': 128,
 }
 
